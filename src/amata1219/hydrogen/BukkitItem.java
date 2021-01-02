eefb24b6f41185ca0e83389023b6927768303f6f
amata1219 <suzukiamata@gmail.com>
2021-01-02 14:57:22 +0900

    :recycle: リファクタリングをし魔界

--- src/amata1219/hydrogen/BukkitItem.java
+++ src/amata1219/hydrogen/BukkitItem.java
@@ -3,17 +3,16 @@ package amata1219.hydrogen;
 import com.google.common.collect.Iterables;
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
-import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.event.player.PlayerItemConsumeEvent;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.Damageable;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.persistence.PersistentDataType;
 
 import java.util.*;
 import java.util.function.Consumer;
 
-public class ExecutableItemStack {
+public class BukkitItem {
 
     public ItemStack baseItemStack;
     public Material type = Material.AIR;
@@ -24,8 +23,6 @@ public class ExecutableItemStack {
     public Map<Enchantment, Integer> enchantments = new HashMap<>();
     public Set<ItemFlag> flags = new HashSet<>();
     public Consumer<ItemStack> raw = Constants.noOperation();
-    public Consumer<PlayerInteractEvent> actionOnClick = Constants.noOperation();
-    public Consumer<PlayerItemConsumeEvent> actionOnConsume = Constants.noOperation();
 
     public void lore(String... lines){
         lore.addAll(Arrays.asList(lines));
@@ -43,26 +40,23 @@ public class ExecutableItemStack {
         this.flags.addAll(Arrays.asList(flags));
     }
 
-    public ItemStack buildItemStack(){
+    public ItemStack build(String id){
         ItemStack item = baseItemStack != null ? baseItemStack : new ItemStack(type);
-        apply(item);
-        return item;
-    }
 
-    private void apply(ItemStack item) {
         item.setAmount(amount);
 
         ItemMeta meta = item.getItemMeta();
-        if(meta != null){
-            if(meta instanceof Damageable) ((Damageable) meta).setDamage(damage);
-            meta.setDisplayName(displayName);
-            meta.setLore(lore);
-            enchantments.forEach((enchantment, level) -> meta.addEnchant(enchantment, level, true));
-            meta.addItemFlags(Iterables.toArray(flags, ItemFlag.class));
-            item.setItemMeta(meta);
-        }
+
+        if(meta instanceof Damageable) ((Damageable) meta).setDamage(damage);
+        meta.setDisplayName(displayName);
+        meta.setLore(lore);
+        enchantments.forEach((enchantment, level) -> meta.addEnchant(enchantment, level, true));
+        meta.addItemFlags(Iterables.toArray(flags, ItemFlag.class));
+        meta.getPersistentDataContainer().set(Constants.executable_item_id(), PersistentDataType.STRING, id);
+        item.setItemMeta(meta);
 
         raw.accept(item);
+        return item;
     }
 
 }
